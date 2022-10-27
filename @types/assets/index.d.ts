5e926c92eeffd919d87b32bbb196b64b8eac37dc
Talgat <tairovtalgat@gmail.com>
2022-10-27 19:08:50 +0600

    migrate from next to simple SPA on vite

--- @types/assets/index.d.ts
+++ @types/assets/index.d.ts
@@ -1,9 +1,21 @@
-declare module '*.svg' {
-    import * as React from 'react';
+declare module "\*.svg" {
+    import React = require("react");
+    export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>;
+    const src: string;
+    export default src;
+}
 
-    const ReactComponent: React.FunctionComponent<
-        React.SVGProps<SVGSVGElement> & { title?: string }
-        >;
+declare module "\*.jpg" {
+    const content: string;
+    export default content;
+}
 
-    export default ReactComponent;
+declare module "\*.png" {
+    const content: string;
+    export default content;
+}
+
+declare module "\*.json" {
+    const content: string;
+    export default content;
 }
---
+++ src/src/pages/index.tsx
@@ -0,0 +1,118 @@
+import type {NextPage} from 'next'
+import styles from '../styles/Home.module.css'
+import {useCallback, useEffect, useRef, useState} from "react";
+import TdClient, {TdObject} from "tdweb"
+import {QrCode} from "./Auth/QrCode";
+import {Phone} from "./Auth/Phone/index.";
+
+import {useTdLib} from "@/shared/api";
+import {TdStates} from "@/constants";
+
+// import tg_logo from "/assets/tg_logo.png"
+
+
+const Home: NextPage = () => {
+    const {client, event, reloadClient} = useTdLib();
+
+
+    const [isLogWithPhone, setIsLogWithPhone] = useState(false);
+
+    console.log("RENDER HOME PAGE", event);
+
+    useEffect(() => {
+        const handleAuth = async () => {
+            try {
+                if (event["@type"] === "updateAuthorizationState") {
+                    const authState = event["authorization_state"] as TdObject;
+                    const type = authState["@type"];
+
+                    /* TODO:
+                        authorizationStateClosing
+                         authorizationStateWaitCode
+                          authorizationStateWaitPassword
+                           authorizationStateWaitRegistration, and authorizationStateWaitTdlibParameters.
+                    */
+                    switch (type) {
+                        case 'authorizationStateClosed':
+                            console.log("START DESTR");
+                            // reloadClient();
+                            // await client?.send({'@type': 'destroy'}); // TODO: нужно?
+                            console.log("DESTROYED");
+
+                            // window.location.reload(); // a kind of a 'hack' but it works...
+                            // console.log("-------------------------authorizationStateClosed -> destroy");
+                            break;
+                        case "authorizationStateWaitEncryptionKey":
+                            console.log("checkDatabaseEncryptionKey send");
+                            await client?.send({
+                                '@type': 'checkDatabaseEncryptionKey'
+                            })
+                            console.log("checkDatabaseEncryptionKey SUCCESS");
+
+                            break;
+                        case 'authorizationStateWaitPhoneNumber':
+                            if (!isLogWithPhone) {
+                                await client?.send({
+                                    '@type': 'requestQrCodeAuthentication',
+                                    other_user_ids: []
+                                });
+                                console.log("SEND");
+                            }
+                            break;
+                        case 'authorizationStateWaitOtherDeviceConfirmation':
+                            break;
+                        case "authorizationStateReady":
+                            // TODO: тут че
+                            break;
+                        default:
+                            break;
+
+                    }
+                }
+            } catch (err) {
+                console.log(err);
+                debugger
+            }
+        };
+
+        if (client && event?.authorization_state)
+            void handleAuth();
+
+    }, [client, event]);
+
+
+    if (event && event["@type"] === "updateAuthorizationState") {
+        const authState = event["authorization_state"] as TdObject;
+        const type = authState["@type"];
+
+
+        if (type === "authorizationStateWaitOtherDeviceConfirmation" && !isLogWithPhone)
+            return <QrCode
+                link={event.authorization_state.link}
+                onPhone={() => {
+                    setIsLogWithPhone(true);
+                }}/>
+        else if (type === TdStates.Auth.authorizationStateWaitPhoneNumber && isLogWithPhone)
+            return <Phone event={event}/>
+    }
+
+    return (
+        <div className={styles.container}>
+            <h1>Lol Kek</h1>
+            <button type="button" onClick={() => {
+                client?.send({
+                    "@type": "logOut"
+                });
+            }}>
+                Log out
+            </button>
+
+            <button type="button" onClick={() => {
+            }}>
+                Log with phone number
+            </button>
+        </div>
+    )
+};
+
+export default Home
