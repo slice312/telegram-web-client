b0aeb95d226bbb5ba1f0d93b23864d43749672ae
Estebes <estebesm95@gmail.com>
2022-11-01 12:14:52 +0600

    feature/chatlist

--- src/components/chatsBar/chatList/ChatList.tsx
+++ src/components/chatsBar/chatList/ChatList.tsx
@@ -1,4 +1,4 @@
-import {List, ListItem, Divider, ListItemText,
+import {List, Badge, ListItem, Divider, ListItemText,
     ListItemAvatar, Avatar, Typography} from "@mui/material";
 
 
@@ -10,48 +10,48 @@ export const ChatList = () => {
                     <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
                 </ListItemAvatar>
                 <ListItemText
-                    primary="Estebes Mukambetov"
-                    secondary={
-                        <>
+                    primary={
+                        <div style={{
+                            display: "flex",
+                            justifyContent: "space-between",
+                            alignItems: "center"
+                        }}>
                             <Typography style={{textOverflow: "ellipsis", overflow: "hidden", whiteSpace: "nowrap"}}>
-                                {" I'll be in your neighborhood oksrg kds rbfo lo rvas"}
+                                {"Estebes Mukambetovdaefaefaefaef"}
                             </Typography>
-                        </>
+                            <Typography>
+                                12.09.2022
+                            </Typography>
+                        </div>
+                    }
+                    secondary={
+                        <div style={{
+                            display: "flex",
+                            justifyContent: "space-between",
+                            alignItems: "center",
+                            paddingTop: 4
+                        }}>
+                            <Typography style={{textOverflow: "ellipsis", overflow: "hidden", whiteSpace: "nowrap"}}>
+                                {"Wish I could come, but I'm out of town this"}
+                            </Typography>
+                            <div style={{
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                                minWidth: 28,
+                                height: 28,
+                                padding: "0 8px",
+                                borderRadius: 14,
+                                backgroundColor: "red",
+                                marginLeft: 6
+                            }}>
+                                <p style={{fontSize: "14px", lineHeight: "12px"}}>49</p>
+                            </div>
+                        </div>
                     }
                 />
             </ListItem>
             <Divider variant="inset" component="li" />
-            <ListItem alignItems="flex-start">
-                <ListItemAvatar>
-                    <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
-                </ListItemAvatar>
-                <ListItemText
-                    primary="Talgat Tairov"
-                    secondary={
-                        <>
-                            <Typography style={{textOverflow: "ellipsis", overflow: "hidden", whiteSpace: "nowrap"}}>
-                                {" I'll be in your neighborhood oksrg kds rbfo lo rvas"}
-                            </Typography>
-                        </>
-                    }
-                />
-            </ListItem>
-            <Divider variant="inset" component="li" />
-            <ListItem alignItems="flex-start">
-                <ListItemAvatar>
-                    <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
-                </ListItemAvatar>
-                <ListItemText
-                    primary="Asel Kurmanbek"
-                    secondary={
-                        <>
-                            <Typography style={{textOverflow: "ellipsis", overflow: "hidden", whiteSpace: "nowrap"}}>
-                                {" I'll be in your neighborhood oksrg kds rbfo lo rvas"}
-                            </Typography>
-                        </>
-                    }
-                />
-            </ListItem>
         </List>
     );
 };
